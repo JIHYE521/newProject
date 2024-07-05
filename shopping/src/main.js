22b410615d99a4dd73341bc39963964894ed5df6
박지혜 <128348123+jijijihyehyehye@users.noreply.github.com>
2024-07-05 16:50:34 +0900

    shop : 장바구니 담기 작업중👷🏻

--- shopping/src/main.js
+++ shopping/src/main.js
@@ -1,48 +1,67 @@
 function getData() {
-	return fetch('/shopping/data/store.json') //
-		.then(response => response.json()) //
-		.then(data => data.products) //
+	return fetch('/shopping/data/store.json')
+		.then(response => response.json())
+		.then(data => data.products)
 		.catch(error => {
 			console.log('error : ', error);
 			throw error;
 		});
 }
 
-getData() //
+getData()
 	.then(products => {
-		//item 생성
-		createItem(products);
+		createItem(products, productContainer);
 		filterItem(products);
-	}) //
+		addCart(products);
+	})
 	.catch(error => {
 		console.log('error2 : ', error);
 	});
 
+const productContainer = document.querySelector('.products__container');
 //item 생성하기
-function createItem(products) {
-	const ul = document.querySelector('.products ul');
-
-	ul.innerHTML = '';
+function createItem(products, productContainer) {
+	productContainer.innerHTML = '';
 	products.forEach(item => {
-		const li = `
-			<li class="product">
+		const div = `
+			<div class="product" data-index=${item.id}>
 				<img src="img/${item.photo}" alt="" />
 				<div>
 					<span class="brand">${item.brand}</span>
 					<p class="title">${item.title}</p>
-					<p class="price">${item.price}</p>
+					<p class="price">₩${item.price}</p>
 				</div>
 				<div class="product__btn">
 					<button type="button" class="btn-add-cart">장바구니</button>
 					<button type="button" class="btn-buy-now">바로구매</button>
 				</div>
-			</li>
+			</div>
 		`;
 
-		ul.insertAdjacentHTML('beforeend', li);
+		productContainer.insertAdjacentHTML('beforeend', div);
 	});
 }
 
+function cartItem(products, productContainer) {
+	productContainer.innerHTML = '';
+	products.forEach(item => {
+		const div = `
+			<div class="product" data-index=${item.id}>
+				<img src="img/${item.photo}" alt="" />
+				<div>
+					<span class="brand">${item.brand}</span>
+					<p class="title">${item.title}</p>
+					<p class="price">₩${item.price}</p>
+				</div>
+				<div class="product__count">${item.quantity}</div>
+			</div>
+		`;
+
+		productContainer.insertAdjacentHTML('beforeend', div);
+	});
+}
+
+// 검색
 const searchInput = document.querySelector('.search input');
 const searchBtn = document.querySelector('.btn-serch');
 
@@ -52,8 +71,29 @@ function filterItem(products) {
 		const filterProducts = products.filter(item => {
 			return item.title.includes(text) || item.brand.includes(text);
 		});
-		createItem(filterProducts);
+		createItem(filterProducts, productContainer);
 	}
 
 	searchBtn.addEventListener('click', filtering);
+}
+
+// 장바구니 담기
+function addCart(products) {
+	const cart = document.querySelector('.cart__drag');
+	let cartProducts = [];
+
+	productContainer.addEventListener('click', e => {
+		const target = e.target;
+		const targetParent = target.parentNode.parentNode;
+		const prdIndex = target.parentNode.parentNode.dataset.index;
+
+		// 클릭하면 담기
+		if (target.matches('.btn-add-cart')) {
+			cartProducts = products.filter(item => item.id === +prdIndex);
+			cartItem(cartProducts, cart);
+		}
+
+		console.log(cartProducts);
+		// 담은거에서 중복확인하기
+	});
 }
