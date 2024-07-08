bd0abe499c83cf22a5a642f7283afb3aa8600b8c
박지혜 <128348123+jijijihyehyehye@users.noreply.github.com>
2024-07-08 10:09:05 +0900

    필터 - fetch data

--- filter/main.js
+++ filter/main.js
@@ -1,1 +1,15 @@
-console.log('👀Test');
+// Fetch the Items from the JSON file
+function loadItems() {
+	return fetch('data/data.json') //
+		.then(response => response.json()) //
+		.then(json => json.items);
+}
+
+// main
+loadItems() //
+	.then(item => {
+		console.log(item);
+		// displayItem(item);
+		// setEventListeners(item);
+	}) //
+	.catch(console.log);
