0ba417a34a4088987a620e4fca637a3df13dbac8
박지혜 <128348123+jijijihyehyehye@users.noreply.github.com>
2024-07-08 10:54:01 +0900

    필터  - feat : setEventListeners

--- filter/main.js
+++ filter/main.js
@@ -9,13 +9,13 @@ function loadItems() {
 loadItems() //
 	.then(item => {
 		displayItem(item);
-		// setEventListeners(item);
+		setEventListeners(item);
 	}) //
 	.catch(console.log);
 
-const ul = document.querySelector('section ul');
-
 function displayItem(item) {
+	const ul = document.querySelector('section ul');
+	ul.innerHTML = '';
 	item.forEach(item => {
 		const li = `
       <li>
@@ -26,4 +26,24 @@ function displayItem(item) {
 
 		ul.insertAdjacentHTML('beforeend', li);
 	});
+}
+
+function setEventListeners(item) {
+	const btns = document.querySelector('.btns');
+
+	btns.addEventListener('click', e => {
+		// console.log(e.target);
+		if (e.target.matches('.btn-color')) {
+			const color = e.target.dataset.color;
+			let filterItem = item.filter(item => item.color === color);
+			displayItem(filterItem);
+		} else if (e.target.matches('.btn-img img')) {
+			const category = e.target.parentNode.dataset.category;
+			filterItem = item.filter(item => item.type === category);
+			displayItem(filterItem);
+		}
+	});
+
+	const btnRefresh = document.querySelector('.btn-refresh');
+	btnRefresh.addEventListener('click', () => displayItem(item));
 }
